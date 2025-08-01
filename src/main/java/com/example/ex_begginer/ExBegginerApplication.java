6ca371a98b15d7f7103e646f1020d22d22dc426f
MASAKI <masaki.segawa@rakus-partners.co.jp>
2025-08-01 11:08:49 +0900

    init

---
+++ src/main/java/com/example/ex_begginer/ExBegginerApplication.java
@@ -0,0 +1,13 @@
+package com.example.ex_begginer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExBegginerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExBegginerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ex_begginer/ExBegginerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ex_begginer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExBegginerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
